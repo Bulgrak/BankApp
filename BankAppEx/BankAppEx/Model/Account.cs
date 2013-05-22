0b0aff52fabfa1bcf57cb46aac25d4620250ec5a
Bulgrak <erik.gravesen@gmail.com>
2013-05-22 13:53:57 +0200

    Created test project

---
+++ BankAppEx/BankAppEx/Model/Account.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAppEx.Model
+{
+    class Account
+    {
+        private double balance;
+        private double interest;
+
+        public Account()
+        {
+        }
+
+        public void WithDraw(double amount)
+        {
+        }
+
+        public void Deposit(double amount)
+        {
+        }
+    }
+}
---
+++ BankAppEx/BankAppExWhiteBoxTest/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BankAppExWhiteBoxTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestWithDraw1()
+        {
+
+        }
+    }
+}
